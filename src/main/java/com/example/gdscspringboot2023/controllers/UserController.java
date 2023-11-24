70adb57a3b2d03da7a46711e95dc05a8b63f3dc5
Mihai Popescu <49340373+mihaipopescu97@users.noreply.github.com>
2023-11-24 21:28:11 +0200

    WS-6

--- src/main/java/com/example/gdscspringboot2023/controllers/UserController.java
+++ src/main/java/com/example/gdscspringboot2023/controllers/UserController.java
@@ -1,17 +1,24 @@
 package com.example.gdscspringboot2023.controllers;
 
+import com.example.gdscspringboot2023.dtos.UserDto;
 import com.example.gdscspringboot2023.entities.User;
 import com.example.gdscspringboot2023.services.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
+@Slf4j
 public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private ModelMapper modelMapper;
 
     @GetMapping("/{id}")
     public ResponseEntity<?> get(@PathVariable final Long id) {
@@ -20,9 +27,22 @@ public class UserController {
     }
 
     @PostMapping
-    public ResponseEntity<?> post(@RequestBody final User user) {
+    public ResponseEntity<?> post(@RequestBody final UserDto userDto) {
+        log.info("UserController -> post() called");
+        final User user = modelMapper.map(userDto, User.class);
         final User createUser = userService.createUser(user);
-        return ResponseEntity.ok(createUser);
+        final UserDto newUserDto = modelMapper.map(createUser, UserDto.class);
+        log.info("UserController -> post() finished");
+        return ResponseEntity.ok(newUserDto);
+    }
+
+    @PutMapping
+    public ResponseEntity<?> put(@RequestBody final UserDto userDto) {
+        log.info("UserController -> put() called");
+        final User updatedUser = userService.updateUser(userDto);
+        final UserDto updatedUserDto = modelMapper.map(updatedUser, UserDto.class);
+        log.info("UserController -> put() finished");
+        return ResponseEntity.ok(updatedUserDto);
     }
 
     @DeleteMapping("/{id}")
---
+++ src/main/java/com/example/gdscspringboot2023/dtos/ProjectDto.java
@@ -0,0 +1,13 @@
+package com.example.gdscspringboot2023.dtos;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ProjectDto {
+
+    private Long id;
+
+    private String name;
+}
